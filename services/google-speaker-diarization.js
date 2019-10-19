1ce5fa15fb03c068f4c2e2e348e9ae74ab889a91
Csignore <ychenvin@gmail.com>
2019-10-19 15:17:20 -0700

    Update googleSpeakerDiarization.js
    able to find long enough audio (as in StartTime and EndTime) for each speaker, need to find a way to split the audio

--- services/google-speaker-diarization.js
+++ services/google-speaker-diarization.js
@@ -8,10 +8,14 @@ const speech = require('@google-cloud/speech').v1p1beta1;
 // Creates a client
 const client = new speech.SpeechClient();
 
+// this is the min length of an audio ppl speaking
+const minLength = 10;
+
 //const fileName = 'OSR_us_000_0010_8k.wav';
 //const fileName = 'testthreeppl.wav';
 const fileName = 'test2.wav';
 const speakerCount = 3;
+var speakersAudio = new Map();
 const config = {
     encoding: 'LINEAR16',
     sampleRateHertz: 8000,
@@ -49,16 +53,56 @@ const getResponse = async () => {
     let prevTag = 0;
     let sentence = "Meeting Minutes\n";
     wordsInfo.forEach((a) => {
+            var timeduration = [parseInt(a.startTime.seconds)+(a.startTime.nanos)/1000000000, parseInt(a.endTime.seconds)+(a.endTime.nanos)/1000000000];
             if (a.speakerTag !== prevTag) {
                 sentence += `\nSpeaker ${a.speakerTag}: ${a.word}`;
+
+
             } else {
                 sentence += ` ${a.word}`;
             }
             prevTag = a.speakerTag;
-        }
-    );
+
+
+            if (speakersAudio.length != speakerCount || !speakersAudio.get(a.speakerTag)
+            || !checklength(speakersAudio.get(a.speakerTag))){
+                if (!speakersAudio.get(a.speakerTag)) {
+                    speakersAudio.set(a.speakerTag, [timeduration]);
+                }
+                else {
+                    var currArrDuration = speakersAudio.get(a.speakerTag);
+                    currArrDuration.push(timeduration);
+                    speakersAudio.set(a.speakerTag, currArrDuration);
+                }
+            }
+           
+    });
+    speakersAudio.forEach(mergeDuration);
+    console.log(speakersAudio);
     return sentence;
 };
+//this function checks a partipant 's audio length is long enough, 
+//eg. [[startTime1, endTime1], [startTime2, endTime2]]
+function checklength(timeDurationArr){
+    var sum = 0;
+    timeDurationArr.forEach((duration)=>{
+        sum += duration[1]-duration[0];
+    })
+
+    return (sum>=minLength);
+}
+
+// function to merge two time duation if they r continue (can be improved)
+function mergeDuration(value){
+    for (let i = 0; i<value.length-1; i++){
+        if (value[i][1] == value[i+1][0]){
+            value[i][1] = value[i+1][1];
+            value.splice(i+1,1);
+            i--;
+        }
+    }
+}
+
 getResponse()
     .then((sentence) => {
         fs.writeFile('MeetingMinutes.txt', sentence, 'utf8', (err) => {
