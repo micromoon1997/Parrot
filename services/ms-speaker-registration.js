d63fd7558093bd774a7141794c4e9e77f97ee827
Adam Yang <yhy1009511320@gmail.com>
2019-11-05 15:20:08 -0800

    Rename azure cognitive service env virables

--- services/ms-speaker-registration.js
+++ services/ms-speaker-registration.js
@@ -1,5 +1,5 @@
-const AZURE_KEY = process.env["AZURE_KEY"];
-const AZURE_ENDPOINT = process.env["AZURE_ENDPOINT"]
+const AZURE_KEY = process.env["AZURE_COGNITIVE_KEY"];
+const AZURE_ENDPOINT = process.env["AZURE_COGNITIVE_ENDPOINT"];
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 let guid;
@@ -51,7 +51,7 @@ function createEnrollment(blob, res) {
             console.log(xhr.status);
             // console.log(xhr.responseText);
         }
-    }
+    };
     xhr.send(blob.buffer);
 }
 
