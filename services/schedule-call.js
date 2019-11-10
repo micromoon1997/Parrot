2c065a7b4085572604b14a6b4ad5e808608cafc4
Kwan <kwan,.lam@alumni.ubc.ca>
2019-11-09 21:15:11 -0800

    feat(services--schedule-call): update scheduleCall to store callSid in database

--- services/schedule-call.js
+++ services/schedule-call.js
@@ -1,23 +1,18 @@
 const schedule = require('node-schedule');
-const makeCall = require("./twilio/make-call");
-const fetchRecordingSid = require("./twilio/fetch-recording-sid");
-const fetchRecording = require("./twilio/fetch-recording");
-
-// TODO: Store in database for testRecording
-let callSid;
+const {makeCall} = require('./twilio');
+const {getDatabase} = require('./database');
 
 const scheduleCall = ({meetingId, time, toPhoneNumber, fromPhoneNumber}) => {
     schedule.scheduleJob(time, async () => {
-      callSid = await makeCall({meetingId, toPhoneNumber, fromPhoneNumber, record: true});
+      const db = getDatabase();
+      const callSid = await makeCall({meetingId, toPhoneNumber, fromPhoneNumber, record: true});
+      db.collection('meetings').updateOne(
+        {meetingId: meetingId},
+        {$set: {callSid} },
+      );
     });
 };
 
-const testRecording = async meetingId => {
-    const recordingSid = await fetchRecordingSid(callSid);
-    await fetchRecording({recordingSid, meetingId});
-};
-
 module.exports = {
-  scheduleCall: scheduleCall,
-  testRecording: testRecording
+  scheduleCall
 };
