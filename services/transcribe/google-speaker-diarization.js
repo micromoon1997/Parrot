ea8bc57d2698031265029ec975cc89cf3f1416f7
Yan Wang <wangyan951027@163.com>
2019-11-10 00:43:57 -0800

    Refactor transcribing functions

--- services/transcribe/google-speaker-diarization.js
+++ services/transcribe/google-speaker-diarization.js
@@ -2,6 +2,8 @@ process.env['GOOGLE_APPLICATION_CREDENTIALS'] = '../../private-key.json';
 
 const fs = require('fs');
 const audioTrim = require('./audioTrim');
+const {getDatabase} = require('../database');
+
 
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech').v1p1beta1;
@@ -12,29 +14,44 @@ const client = new speech.SpeechClient();
 // this is the min length of an audio ppl speaking
 
 
-const fileName = './testAudio/test2.wav';
-const speakerCount = 3;
-const speakersAudio = new Map();
-const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 8000,
-    languageCode: `en-US`,
-    enableSpeakerDiarization: true,
-    diarizationSpeakerCount: speakerCount,
-    model: 'phone_call',
-    enableAutomaticPunctuation: true,
-};
-//const uri = 'file://C:/Users/MSI/OneDrive/UBC/CPSC_319/sandbox/google-speaker-diarization//conversation.wav';
-const audio = {
-    //uri: uri,
-    content: fs.readFileSync(fileName).toString('base64'),
-};
-const request = {
-    config: config,
-    audio: audio,
-};
+// const fileName = './testAudio/test2.wav';
+// const speakerCount = 3;
+// const speakersAudio = new Map();
+// const config = {
+//     encoding: 'LINEAR16',
+//     sampleRateHertz: 8000,
+//     languageCode: `en-US`,
+//     enableSpeakerDiarization: true,
+//     diarizationSpeakerCount: speakerCount,
+//     model: 'phone_call',
+//     enableAutomaticPunctuation: true,
+// };
+// //const uri = 'file://C:/Users/MSI/OneDrive/UBC/CPSC_319/sandbox/google-speaker-diarization//conversation.wav';
+// const audio = {
+//     //uri: uri,
+//     content: fs.readFileSync(fileName).toString('base64'),
+// };
+// const request = {
+//     config: config,
+//     audio: audio,
+// };
 
-const getResponse = async () => {
+async function getProfileIDs(meetingId) {
+    const db = getDatabase();
+    const meeting = await db.collection('meetings').findOne({meetingId: meetingId});
+    const profileIds = [];
+    for (const participant of meeting.participants) {
+        const email = participant.emailAddress.address;
+        const query = {email: email};
+        const person = await db.collection('people').findOne(query);
+        profileIds.push(person.ASRGuid);
+        // TODO handle un-enrolled participants
+    }
+    return profileIds;
+}
+
+
+async function getResponse() {
     // const [operation] = await client.longRunningRecognize(request);
     // const [response] = await operation.promise();
 
@@ -50,11 +67,11 @@ const getResponse = async () => {
 // from all the results thus far. Thus, to get all the words with speaker
 // tags, you only have to take the words list from the last result:
     let prevTag = 0;
-    let sentence = "Meeting Minutes\n";
+    let sentence = 'Meeting Minutes\n';
     wordsInfo.forEach((a) => {
         let timeduration = [parseInt(a.startTime.seconds) + (a.startTime.nanos) / 1000000000, parseInt(a.endTime.seconds) + (a.endTime.nanos) / 1000000000];
         if (a.speakerTag !== prevTag) {
-            sentence += `\nSpeaker ${a.speakerTag}: ${a.word}`;
+            sentence += `\nSpeaker${a.speakerTag}: ${a.word}`;
         } else {
             sentence += ` ${a.word}`;
         }
@@ -83,15 +100,20 @@ const getResponse = async () => {
         await audioTrim.getSpeakersSample(value, key);
     }
     return sentence;
-};
+}
 
-getResponse()
-    .then((sentence) => {
-        fs.writeFile('./output/MeetingMinutes.txt', sentence, 'utf8', (err) => {
-            if (err) throw err;
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+
+module.exports = {
+    getProfileIDs: getProfileIDs
+};
+//
+// getResponse()
+//     .then((sentence) => {
+//         fs.writeFile('./output/MeetingMinutes.txt', sentence, 'utf8', (err) => {
+//             if (err) throw err;
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
