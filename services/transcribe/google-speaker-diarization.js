0636c01e6f1995c1531a379a394f962b96c42427
Adam Yang <yhy1009511320@gmail.com>
2019-11-10 22:25:27 -0800

    chore(transcribe--google-speaker-diarization): clean up and rename function to a more clear name

--- services/transcribe/google-speaker-diarization.js
+++ services/transcribe/google-speaker-diarization.js
@@ -2,7 +2,7 @@ process.env['GOOGLE_APPLICATION_CREDENTIALS'] = '../../private-key.json';
 
 const fs = require('fs');
 const audioTrim = require('./audioTrim');
-const {getDatabase} = require('../database');
+const { getDatabase } = require('../database');
 
 
 // Imports the Google Cloud client library
@@ -10,51 +10,30 @@ const speech = require('@google-cloud/speech').v1p1beta1;
 
 // Creates a client
 const client = new speech.SpeechClient();
+const speakersAudio = new Map();
+//const uri = 'file://C:/Users/MSI/OneDrive/UBC/CPSC_319/sandbox/google-speaker-diarization//conversation.wav';
 
-// this is the min length of an audio ppl speaking
-
-
-// const fileName = './testAudio/test2.wav';
-// const speakerCount = 3;
-// const speakersAudio = new Map();
-// const config = {
-//     encoding: 'LINEAR16',
-//     sampleRateHertz: 8000,
-//     languageCode: `en-US`,
-//     enableSpeakerDiarization: true,
-//     diarizationSpeakerCount: speakerCount,
-//     model: 'phone_call',
-//     enableAutomaticPunctuation: true,
-// };
-// //const uri = 'file://C:/Users/MSI/OneDrive/UBC/CPSC_319/sandbox/google-speaker-diarization//conversation.wav';
-// const audio = {
-//     //uri: uri,
-//     content: fs.readFileSync(fileName).toString('base64'),
-// };
-// const request = {
-//     config: config,
-//     audio: audio,
-// };
-
-async function getProfileIDs(meetingId) {
-    const db = getDatabase();
-    const meeting = await db.collection('meetings').findOne({meetingId: meetingId});
-    const profileIds = [];
-    for (const participant of meeting.participants) {
-        const email = participant.emailAddress.address;
-        const query = {email: email};
-        const person = await db.collection('people').findOne(query);
-        profileIds.push(person.ASRGuid);
-        // TODO handle un-enrolled participants
-    }
-    return profileIds;
-}
-
-
-async function getResponse() {
+async function getUntaggedTranscription(recordingFileUrl, speakerCount) {
     // const [operation] = await client.longRunningRecognize(request);
     // const [response] = await operation.promise();
+    const audio = {
+    //uri: uri,
+        content: fs.readFileSync(recordingFileUrl).toString('base64')
+    };
+    const config = {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 8000,
+        languageCode: `en-US`,
+        enableSpeakerDiarization: true,
+        diarizationSpeakerCount: speakerCount,
+        model: 'phone_call',
+        enableAutomaticPunctuation: true,
+    };
 
+    const request = {
+        config: config,
+        audio: audio,
+    };
     const [response] = await client.recognize(request);
     const transcription = response.results
         .map(result => result.alternatives[0].transcript)
@@ -62,16 +41,16 @@ async function getResponse() {
     //console.log(`Transcription: ${transcription}`);
     const result = response.results[response.results.length - 1];
     const wordsInfo = result.alternatives[0].words;
-// Note: The transcript within each result is separate and sequential per result.
-// However, the words list within an alternative includes all the words
-// from all the results thus far. Thus, to get all the words with speaker
-// tags, you only have to take the words list from the last result:
+    // Note: The transcript within each result is separate and sequential per result.
+    // However, the words list within an alternative includes all the words
+    // from all the results thus far. Thus, to get all the words with speaker
+    // tags, you only have to take the words list from the last result:
     let prevTag = 0;
     let sentence = 'Meeting Minutes\n';
     wordsInfo.forEach((a) => {
-        let timeduration = [parseInt(a.startTime.seconds) + (a.startTime.nanos) / 1000000000, parseInt(a.endTime.seconds) + (a.endTime.nanos) / 1000000000];
+        let timeDuration = [parseInt(a.startTime.seconds) + (a.startTime.nanos) / 1000000000, parseInt(a.endTime.seconds) + (a.endTime.nanos) / 1000000000];
         if (a.speakerTag !== prevTag) {
-            sentence += `\nSpeaker${a.speakerTag}: ${a.word}`;
+            sentence += `\nspeaker${a.speakerTag}: ${a.word}`;
         } else {
             sentence += ` ${a.word}`;
         }
@@ -80,10 +59,10 @@ async function getResponse() {
         if (speakersAudio.length !== speakerCount || !speakersAudio.get(a.speakerTag)
             || !audioTrim.checkAudioLength(speakersAudio.get(a.speakerTag))) {
             if (!speakersAudio.get(a.speakerTag)) {
-                speakersAudio.set(a.speakerTag, [timeduration]);
+                speakersAudio.set(a.speakerTag, [timeDuration]);
             } else {
-                var currArrDuration = speakersAudio.get(a.speakerTag);
-                currArrDuration.push(timeduration);
+                const currArrDuration = speakersAudio.get(a.speakerTag);
+                currArrDuration.push(timeDuration);
                 speakersAudio.set(a.speakerTag, currArrDuration);
             }
         }
@@ -104,16 +83,6 @@ async function getResponse() {
 
 
 module.exports = {
-    getProfileIDs: getProfileIDs
+    getUntaggedTranscription: getUntaggedTranscription
 };
-//
-// getResponse()
-//     .then((sentence) => {
-//         fs.writeFile('./output/MeetingMinutes.txt', sentence, 'utf8', (err) => {
-//             if (err) throw err;
-//         });
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
 
