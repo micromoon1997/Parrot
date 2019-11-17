7e9b624d0ce15e2072c625bf04e27fa9bad2acd3
chid123 <chid123@students.cs.ubc.ca>
2019-11-17 13:16:25 -0800

    update package.json

---
+++ specs/services/ms-speaker-registration.spec.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect  = chai.expect;
+
+describe("test test", function (){
+    it('test', function(done){
+        expect(0).to.equal(0);
+        done();
+    })
+})
