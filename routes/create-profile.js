5ac73abcde75dd2af69ca7eacd855eba73832e2a
Adam Yang <yhy1009511320@gmail.com>
2019-11-19 18:17:25 -0800

    refactor(services/ms-speaker-registration.js): refactor to use axios

--- routes/create-profile.js
+++ routes/create-profile.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const registration = require('../services/ms-speaker-registration.js');
 
-router.post('/', function(req, res,next) {
-    registration.createProfile(res);
+router.post('/', async function(req, res,next) {
+    try {
+        const profileId = await registration.createProfile();
+
+    } catch (err) {
+        console.log('Fail to create profile:');
+        console.log(err);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
