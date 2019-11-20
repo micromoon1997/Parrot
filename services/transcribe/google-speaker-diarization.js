2cc2af1dd7ef94c9f016352f47ead3dde0185561
Csignore <ychenvin@gmail.com>
2019-11-19 17:12:30 -0800

    Refactor clearfiles func

--- services/transcribe/google-speaker-diarization.js
+++ services/transcribe/google-speaker-diarization.js
@@ -2,8 +2,6 @@ process.env['GOOGLE_APPLICATION_CREDENTIALS'] = `${__dirname}/../../private-key.
 
 const audioTrim = require('./audioTrim');
 const bucketName = 'untranscribed';
-const fs = require('fs');
-const path = require('path');
 
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech').v1p1beta1;
@@ -95,16 +93,6 @@ async function getUntaggedTranscription(meetingId, speakerCount) {
     for (let [key, value] of speakersAudio) {
         await audioTrim.getSpeakersSample(value, key);
     }
-    const directory  = `${__dirname}/output/tempdir`
-    fs.readdir(directory, (err, files) => {
-        if (err) throw err;
-
-        for (const file of files) {
-            fs.unlink(path.join(directory, file), err => {
-                if (err) throw err;
-            });
-        }
-    });
 
     console.log(sentence);
     return sentence;
