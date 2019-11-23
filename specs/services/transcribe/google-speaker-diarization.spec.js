81b243330f3ef3a89b2b5f8ff75d4797165af4fa
Yan Wang <wangyan951027@163.com>
2019-11-23 12:12:39 -0800

    rebase from master

--- specs/services/transcribe/google-speaker-diarization.spec.js
+++ specs/services/transcribe/google-speaker-diarization.spec.js
@@ -1,25 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const googleClient = require('../../../services/transcribe/google-speaker-diarization');
-
-function removeFilesFromFolder(dirPath) {
-    fs.readdir(dirPath, (err, files) => {
-        if (err) throw err;
-
-        for (const file of files) {
-            const filePath = path.join(dirPath, file);
-            if (fs.statSync(filePath).isFile()) {
-                fs.unlinkSync(filePath);
-            }
-        }
-    });
-}
+const removeHelper = require('../../../services/transcribe/clear-temporary-files');
 
 describe('Google client', function () {
-    this.timeout(30000);
     after(function () {
-        removeFilesFromFolder(`${__dirname}/../../../services/transcribe/output`);
-        removeFilesFromFolder(`${__dirname}/../../../services/transcribe/output/tempdir`);
+        removeHelper.clearWavFile(`${__dirname}/../../../tmp`);
+        // removeFilesFromFolder(`${__dirname}/../../../services/transcribe/output`);
+        // removeFilesFromFolder(`${__dirname}/../../../services/transcribe/output/tempdir`);
     });
 
     it('should get untagged transcription', async function () {
