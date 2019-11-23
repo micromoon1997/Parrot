63fb383b69f0084d3f14cf882b7f0fa3544b1ea8
Adam Yang <yhy1009511320@gmail.com>
2019-11-22 20:10:47 -0800

    c3/refactor create enrollment to use axios (#16)
    * refactor(services/ms-speaker-registration.js): refactor to use axios
    
    * refactor(services/ms-speaker-registration.js): refactor to use axios
    
    * consolidating create profile into enrollment
    
    * consolidate db update method to register voice
    
    * a bit clean up
    
    * update error handling

--- routes/create-profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const registration = require('../services/ms-speaker-registration.js');
-
-router.post('/', function(req, res,next) {
-    registration.createProfile(res);
-});
-
-module.exports = router;
