0da8c3a33114479e95ab63f0f65461d0f84fd040
Yan Wang <wangyan951027@163.com>
2019-11-23 14:14:08 -0800

    unit tests for google speaker diarization client

--- specs/services/transcribe/google-speaker-diarization.spec.js
+++ specs/services/transcribe/google-speaker-diarization.spec.js
@@ -1,19 +1,22 @@
+const expect = require('chai').expect;
 const googleClient = require('../../../services/transcribe/google-speaker-diarization');
 const {clearWavFile} = require('../../../services/transcribe/clear-temporary-files');
 
 describe('Google client', function () {
     after(function () {
-        clearWavFile(`${__dirname}/../../../tmp`);
+        clearWavFile(`${__appRoot}/tmp`);
     });
 
     it('should get untagged transcription', async function () {
         const meetingId = 'test2.wav';
         const speakerCount = 3;
         const untaggedTranscription = await googleClient.getUntaggedTranscription(meetingId, speakerCount);
-        console.log(untaggedTranscription);
+        expect(untaggedTranscription).to.be.a('string');
     });
 
     it('should create google cloud write stream', function () {
-        // TODO
+        const stream = googleClient.createGoogleCloudWriteStream('123');
+        expect(stream).to.be.an('object');
+        expect(stream).to.have.a.property('writable', true);
     })
 });
