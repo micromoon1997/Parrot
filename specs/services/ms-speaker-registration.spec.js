8f3693322633ef09381d85cace946c555d3149e3
chid123 <chid123@students.cs.ubc.ca>
2019-11-26 00:58:11 -0800

    delete tag transcription test

--- specs/services/ms-speaker-registration.spec.js
+++ specs/services/ms-speaker-registration.spec.js
@@ -1,4 +1,3 @@
-const azureClient = require('../../services/ms-speaker-registration');
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
@@ -10,23 +9,6 @@ const AZURE_KEY = process.env["AZURE_COGNITIVE_KEY"];
 const AZURE_ENDPOINT = process.env["AZURE_COGNITIVE_ENDPOINT"];
 
 chai.use(chaiHttp);
-
-describe("Azure cognitive service client", function () {
-    it('should tag transcription', async function () {
-        const meetingId = 'test2';
-        // Vincent, Yan and Desta respectively
-        const profileIds = ['4d83796d-bcce-4664-b336-7b80a25f11b6', 'b84adae5-881b-4dc4-b2ae-4c18b35839f7', 'b84adae5-881b-4dc4-b2ae-4c18b35839f7'];
-        const untaggedTranscription = 'Meeting Minutes\n' +
-            '\n' +
-            'speaker3: Good morning Professor Austin early\n' +
-            'speaker2: morning James. I\'m doing well, and you I\'m great.\n' +
-            'speaker3: Thank you. This is my friend Emma. She\'s thinking about applying to this college. She has a few questions. Would you mind solely us about this process, please?\n' +
-            'speaker2: Hello Emma. It\'s presently 2. I\'m more than happy to we speak with you. Please stop by my office next week\n' +
-            'speaker1: and Sophia to meet you faster. Thank you so much for helping\n' +
-            'speaker2: the message. Hopefully I will be able to answer over your question.\n';
-        // await azureClient.tagTranscription(meetingId, profileIds, untaggedTranscription);
-    })
-});
 
 describe("createProfile", () => {
     it("It should create profile", () => {
@@ -68,30 +50,3 @@ describe("createProfile", () => {
             });
     });
 });
-
-// describe('createEnrollment', () => {
-//     let guid = '49a36324-fc4b-4387-aa06-090cfbf0064f';
-//     it('should create enrollment with a valid audio', () => {
-//         console.log(AZURE_ENDPOINT);
-//         nock(AZURE_ENDPOINT)
-//         .post(`/identificationProfiles/${guid}/enroll`)
-//         .reply(200, {
-//             "status": "succeeded",
-//             "processingResult": 
-//             {
-//                 "enrollmentStatus" : "Enrolling",
-//                 "remainingEnrollmentSpeechTime" : 15.0,
-//                 "speechTime" : 15.0,
-//                 "enrollmentSpeechTime":15.0
-//             }
-//         });
-
-//         chai.request(ADDRESS)
-//             .post('/enroll')
-//             .attach('voice_sample', fs.readFileSync('../test-audio/10sec_valid_test.wav'))
-//             .end((err,res) => {
-//                 expect(res.statusCode).to.equal(200);
-//                 expect(res.body.status).to.equal('succeeded');
-//             });
-//     });
-// });
